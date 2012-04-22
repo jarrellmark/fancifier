98cbe77c78ea4ff3cae67a79436689e08a023ff3
Koushik Dasika <kdasika@koushikdasika.com>
2012-04-22 07:24:07 -0400

    fixed error in return twitter post

--- getTwitter.php
+++ getTwitter.php
@@ -3,9 +3,8 @@
 		$input = "cats";
 		$twResults = json_decode(file_get_contents("http://search.twitter.com/search.json?q=" .
 				rawurlencode($input) .
-				"&result_type=mixed&count=10&lang=en"));
-		
-		return $twResults->results[0]->text;
-			
+				"&result_type=mixed&count=100&lang=en"));
+		$random = rand(0, 100);
+		return $twResults->results[$random]->text;
 	}
 ?>
