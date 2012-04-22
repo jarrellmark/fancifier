bb69bd9f6001988e789496a1b7ab8869d7ad522b
Koushik Dasika <kdasika@koushikdasika.com>
2012-04-22 07:21:37 -0400

    fixed error in return twitter post

--- getTwitter.php
+++ getTwitter.php
@@ -1,6 +1,6 @@
 <?php
 	function getTwitterPost(){
-		$input = "hackRU";
+		$input = "cats";
 		$twResults = json_decode(file_get_contents("http://search.twitter.com/search.json?q=" .
 				rawurlencode($input) .
 				"&result_type=mixed&count=10&lang=en"));
