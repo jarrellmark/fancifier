dd304eba4853b1e5157fd46671334a595588f1fd
Koushik Dasika <kdasika@koushikdasika.com>
2012-04-22 07:57:38 -0400

    changed the range of the random number generator

--- getTwitter.php
+++ getTwitter.php
@@ -4,7 +4,7 @@
 		$twResults = json_decode(file_get_contents("http://search.twitter.com/search.json?q=" .
 				rawurlencode($input) .
 				"&result_type=mixed&count=20&lang=en"));
-		$random = rand(0, sizeof($twResults));
+		$random = rand(0, sizeof($twResults) - 1);
 		return $twResults->results[$random]->text;
 		
 	}
