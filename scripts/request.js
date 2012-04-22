0057e1e953481d473ba23b2423ffb2fd9045ec5e
Matt <mattj52990@Matt-Jenningss-MacBook-Pro.local>
2012-04-22 00:29:34 -0400

    fixed jquery hopefully

--- scripts/request.js
+++ scripts/request.js
@@ -1,12 +1,16 @@
 <!--
 $(document).ready(function(){
 
-   	  var input = $('#text').attr('value'); 
+   	 /* var input = $('#text').attr('value'); 
    	  var type = $('input:radio[name=fancyType]:checked').val();
  	  
  	  alert("success!");
- 	   	  
-      $.ajax({
+*/
+$('#button').submit(function() {
+  alert('Handler for .submit() called.');
+  return false;
+});
+     /* $.ajax({
         type: "POST",
         url:  "fancify.php",
         data: "type=" + type + "&input=" + input,
@@ -16,6 +20,6 @@ $(document).ready(function(){
       });
 	  
 	  return false;
-	});
+	});*/
 });
 -->
