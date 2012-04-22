2c1d0605d2edeb671588785f6bbc96a3bb9ece35
Koushik Dasika <kdasika@koushikdasika.com>
2012-04-22 07:19:31 -0400

    made the getTwitter.php function so that it returns the first tweet

--- getTwitter.php
+++ getTwitter.php
@@ -1,3 +1,11 @@
 <?php
-
+	function getTwitterPost(){
+		$input = "hackRU";
+		$twResults = json_decode(file_get_contents("http://search.twitter.com/search.json?q=" .
+				rawurlencode($input) .
+				"&result_type=mixed&count=10&lang=en"));
+		
+		echo $twResults->results[0]->text;
+			
+	}
 ?>
